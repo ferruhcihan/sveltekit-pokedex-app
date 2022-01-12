1296cfc60c8b1a8f26ac7944964f62bc789200a0
frcihan <ferruhcihan20@gmail.com>
2022-01-12 04:02:30 +0300

    feat:add about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About the SvelteKit Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This is a really cool SvelteKit demo that uses the Pokemon API to build a Pokedex</p>
